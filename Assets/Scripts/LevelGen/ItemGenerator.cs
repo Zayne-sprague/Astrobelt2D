339ff225dd9ae38a2c3a3d8da7427ed1bf4038d2
zayne sprague <zayne_sprague@yahoo.com>
2019-07-30 22:00:14 -0500

    Have the star goal at the end of a corridor
    You can't leave early into the previous room without the collapse starting to happen now.

--- Assets/Scripts/LevelGen/ItemGenerator.cs
+++ Assets/Scripts/LevelGen/ItemGenerator.cs
@@ -28,7 +28,7 @@ public class ItemGenerator : MonoBehaviour
         float offset_from_edge = 0.5f;
         CorridorGoal inst;
 
-        float length = (corridor.corridorLength / 2f) - offset_from_edge;
+        float length = (corridor.corridorLength - 1) - offset_from_edge;
         float x = corridor.x;
         float y = corridor.y;
 
@@ -43,19 +43,19 @@ public class ItemGenerator : MonoBehaviour
         {
             case Direction.North:
                 inst = Instantiate(prefab, new Vector3(x, (float)(y + length), 0), Quaternion.identity);
-                inst.BuildCollisionBox(1 + (edge_padding * 2), corridor.corridorLength);
+                inst.BuildCollisionBox(1 + (edge_padding * 2), 1);
                 break;
             case Direction.East:
                 inst = Instantiate(prefab, new Vector3((float)(x + length), y, 0), Quaternion.identity);
-                inst.BuildCollisionBox(corridor.corridorLength, 1 + (edge_padding * 2));
+                inst.BuildCollisionBox(1, 1 + (edge_padding * 2));
                 break;
             case Direction.South:
                 inst = Instantiate(prefab, new Vector3(x, (float)(y + length), 0), Quaternion.identity);
-                inst.BuildCollisionBox(1 + (edge_padding * 2), corridor.corridorLength);
+                inst.BuildCollisionBox(1 + (edge_padding * 2), 1);
                 break;
             default: //west
                 inst = Instantiate(prefab, new Vector3((float)(x + length), corridor.y, 0), Quaternion.identity);
-                inst.BuildCollisionBox(corridor.corridorLength, 1 + (edge_padding * 2));
+                inst.BuildCollisionBox(1, 1 + (edge_padding * 2));
                 break;
         }
 
